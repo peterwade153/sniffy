b68a303cdc2e1d946ed8883688558ff992227b36
peterwade153 <peterwade153@gmail.com>
2018-10-30 20:14:13 +0300

    Add crawler func

---
+++ app/sniff.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import ssl
+
+
+url = 'http://honorsprogram.gwublogs.com/2017/11/08/honorsproblems-ive-messed-up-everything-oh-no/'
+
+class Sniff():
+
+    def __init__(self, url):
+        self.url = url
+
+    def fetch_data(self):
+        """
+        Fetches data from a webpage and parse it then pick out the text. 
+        """
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+        req = Request(self.url, headers={'User-Agent': 'Mozilla/5.0'})
+        response = urlopen(req, context=context)
+        raw_text = response.read()
+
+        # soup = BeautifulSoup(raw_text)
+        soup = BeautifulSoup(raw_text, "html5lib")
+        
+        #extract text and remove whitespaces from bit of text
+        sample_text = soup.find("div", 'post-body entry-content')
+        return sample_text
+
+
+if __name__ == '__main__':
+    url = 'http://honorsprogram.gwublogs.com/2017/11/08/honorsproblems-ive-messed-up-everything-oh-no/'
+    url2 = "https://www.wintellect.com/containerize-python-app-5-minutes/"
+    url3 = "http://encouragementforeverydaystruggles.blogspot.com/"
+    sd = Sniff(url3)
+    ls = sd.fetch_data()
+    print(ls)
