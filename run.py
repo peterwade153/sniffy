32276a70177cf14d312608b19d0d2fc45de1371a
peterwade153 <peterwade153@gmail.com>
2018-11-09 19:20:00 +0300

    update run.py

--- run.py
+++ run.py
@@ -2,8 +2,6 @@ from app.sniff import Sniff
 from app.db_handler.db_setup import Database_handler
 
 if __name__ == '__main__':
-    url = 'http://honorsprogram.gwublogs.com/2017/11/08/honorsproblems-ive-messed-up-everything-oh-no/'
-    url2 = "https://www.wintellect.com/containerize-python-app-5-minutes/"
     url3 = "http://encouragementforeverydaystruggles.blogspot.com/"
     sd = Sniff(url3)
     data = sd.fetch_data()
